6ef55fed62ab56c46afdea3512161ec1fda069d7
Hudongzhiti Deploy <deploy@hudongzhiti.local>
2026-07-07 00:43:11 +0800

    Fix static asset URL for admin

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -81,7 +81,7 @@ TIME_ZONE = "Asia/Shanghai"
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STORAGES = {
     "staticfiles": {
